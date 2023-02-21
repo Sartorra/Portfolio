498f7b5344e29962d58ff3e38db213f6d45f9426
Styxies <naniisamod@gmail.com>
2023-02-21 11:01:37 -0500

    Anchor scrolling and about.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -13,21 +13,23 @@ export interface CardInfo {
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
-      if (matches) {
+      let isHandset : boolean = matches;
+
+      if (isHandset) {
         return [
-          { title: 'Card 1', cols: 2, rows: 2, content: '<b>Card content!</b>' },
-          { title: 'Card 2', cols: 2, rows: 2, content: '<b>Card content!</b>' },
+          { title: 'Card 1', cols: 4, rows: 1, content: '<b>Card content!</b>' },
+          { title: 'Card 2', cols: 4, rows: 1, content: '<b>Card content!</b>' },
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 3, rows: 2, content: '<b>Card content!</b>' },
-        { title: 'Card 2', cols: 1, rows: 2, content: '<b>Card content!</b>' },
+        { title: 'Card 1', cols: 3, rows: 1, content: '<b>Card content!</b>' },
+        { title: 'Card 2', cols: 1, rows: 1, content: '<b>Card content!</b>' },
       ];
     })
   );
